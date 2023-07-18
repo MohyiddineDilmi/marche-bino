c819bc136f605af73c87912b8d013f37266bf5fe
Mr104 <33746487+MohyiddineDilmi@users.noreply.github.com>
2023-07-18 19:28:27 -0400

    Update ImagePlayer.jsx

--- src/pages/components/ImagePlayer.jsx
+++ src/pages/components/ImagePlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect }, React from "react";
 
 const ImagePlayer = ({ images, duration }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
