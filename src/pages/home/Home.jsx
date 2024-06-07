ff1418d3b9fa0d9309a22b25f73b6c3c6e4d5f4e
Dilmi <33746487+MohyiddineDilmi@users.noreply.github.com>
2024-06-06 22:38:39 -0400

    bg fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,11 +5,40 @@ import './home.css';
 import ServicesList from './ServicesList';
 import Footer from './Footer';
 import Technologies from './Technologies';
+import {
+  useScroll,
+  useTransform,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  animate,
+} from 'framer-motion';
+import { useRef, useEffect } from 'react';
 
 const vBg =
   'https://raw.githubusercontent.com/MohyiddineDilmi/data/main/videos/full_bg.mp4';
 
 function Home() {
+
+  const COLORS = [
+    '#AD00FF',
+    '#00FFD1',
+    '#00D1FF',
+    '#FF6F07',
+  ];
+
+  const color = useMotionValue(COLORS[0]);
+  const backgroundImage = useMotionTemplate`radial-gradient(200% 150% at 0% 10%, #000000 50%, ${color})`;
+
+  useEffect(() => {
+    animate(color, COLORS, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror',
+    });
+  }, []);
+
   return (
     <>
       <div
@@ -17,25 +46,33 @@ function Home() {
           backgroundColor: 'black',
         }}
       >
-        <div className="container">
-          <div className="overlayStyle"></div>
-          <video className="videoStyle" src={vBg} autoPlay loop muted />
-          <div
-            className="header-content"
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              top: '0',
-            }}
-          >
-            <AppBar />
-            <Header />
+        <motion.div
+          style={{
+            background: backgroundImage,
+            minHeight: '100vh',
+            width: '100%',
+          }}
+        >
+          <div className="container">
+            <div className="overlayStyle"></div>
+            <video className="videoStyle" src={vBg} autoPlay loop muted />
+            <div
+              className="header-content"
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                top: '0',
+              }}
+            >
+              <AppBar />
+              <Header />
+            </div>
           </div>
-        </div>
-        <ServicesList />
-        <Technologies />
-        <Footer />
+          <ServicesList />
+          <Technologies />
+          <Footer />
+        </motion.div>
       </div>
     </>
   );
