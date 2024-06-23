ead4bd087788677a43fdb198edc9131e20a3f6ba
Dilmi <33746487+MohyiddineDilmi@users.noreply.github.com>
2024-06-23 15:44:35 -0400

    visual art

--- src/pages/services/Portfolio.jsx
+++ src/pages/services/Portfolio.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import VideoPlayerCard from '../../components/VideoPlayerCard';
 import styles from '../../modules/styles.module.css';
 import { useTranslation } from 'react-i18next';
-import { padding } from '@mui/system';
 
 
 const portfolioContainerStyle = {
@@ -11,7 +10,7 @@ const portfolioContainerStyle = {
   flexWrap: 'wrap',
   justifyContent: 'center',
   alignItems: 'center',
-  padding: '5rem 0rem 5rem 0rem'
+  padding: '0rem 0rem 5rem 0rem'
 };
 
 function Portfolio() {
@@ -62,7 +61,7 @@ function Portfolio() {
             poster="https://raw.githubusercontent.com/MohyiddineDilmi/data/main/images/__i_0.png"
         />
     </div>
-    <h1 className={styles.title_secondary}>{t('fpv')}</h1>
+    {/* <h1 className={styles.title_secondary}>{t('fpv')}</h1>
     <p className={styles.text}>{t('fpv_porfolio_description')}</p>
     <div style={portfolioContainerStyle}>
     <VideoPlayerCard
@@ -105,7 +104,7 @@ function Portfolio() {
             src="https://raw.githubusercontent.com/MohyiddineDilmi/data/main/videos/v-7_resized.mp4"
             poster="https://raw.githubusercontent.com/MohyiddineDilmi/data/main/images/__i_0.png"
         />
-    </div>
+    </div> */}
 
     </>
     
